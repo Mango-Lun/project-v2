7ce04f7ec9feb221264677cfc608b9a7e0f8287b
Mango-Lun <Mango-Lun@qq.com>
2022-09-27 15:29:02 +0800

    修改样式

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>hello</h1>
+    <el-button>hello</el-button>
+    <el-button type="primary">hello</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.hello {
+  background: yellow;
+}
+</style>
