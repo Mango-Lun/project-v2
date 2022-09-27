7c2c7bd00ec1bef986279e1c941ad222b2f4f331
Mango-Lun <Mango-Lun@qq.com>
2022-09-27 21:00:59 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import '../plugins/element'
 import 'font-awesome/css/font-awesome.min.css'
+import axios from 'axios'
+
+// 挂载到原型，可以全局使用 axios
+Vue.prototype.axios = axios
 
 Vue.config.productionTip = false
 
