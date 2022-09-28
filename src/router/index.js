a9d4403d9bac85f5957c453f93daae6a47b38fa1
Mango-Lun <Mango-Lun@qq.com>
2022-09-28 12:41:31 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../components/Home.vue'
+// import Home from '../components/Home.vue'
 
 Vue.use(Router)
 
@@ -8,7 +8,10 @@ export default new Router({
   routes: [
     {
       path: '/home',
-      component: Home
+      // component: Home
+      // 路由懒加载
+      // component: () => import('@/components/Home') //官方推荐
+      component: resolve => require(['../components/Home'], resolve) //异步组件
     }
   ],
   mode: 'history'
